227a21e35d81a5e9e8f8af3d03cbad4c013f3ff0
HoaiAn0906 <phoaianvlogs@gmail.com>
2024-03-28 23:57:00 +0700

    Feat: update

--- pages/auth/Maintenance.vue
+++ pages/auth/Maintenance.vue
@@ -4,7 +4,6 @@
       <img alt="under_construction" max-width="500" src="/images/backgrounds/maintenance.svg" />
       <h1 class="text-h1 mt-4">Chế độ đang được phát triển!!!</h1>
       <h4 class="text-h4 my-8">Trang web đang được xây dựng. Hãy quay lại sau!</h4>
-      <v-btn class="mb-4" color="primary" flat to="/">Quay lại trang chủ</v-btn>
     </div>
   </div>
 </template>
