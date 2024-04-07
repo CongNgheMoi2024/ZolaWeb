3493f581d86e05cd0ec65868cc4664c3afa60106
Phan Hoai An <phoaianvlogs@gmail.com>
2024-04-07 16:59:05 +0700

    update

--- plugins/sockjs-client.js
+++ plugins/sockjs-client.js
@@ -3,7 +3,7 @@ import Stomp from 'stompjs'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const sock = new SockJS('http://localhost:8080/ws')
+  const sock = new SockJS('http://16.162.190.21/ws')
   const stompClient = Stomp.over(sock)
   nuxtApp.provide('stompClient', stompClient)
 })
