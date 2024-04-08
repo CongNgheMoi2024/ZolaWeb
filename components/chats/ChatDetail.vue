d4c478c10cfb28f4d7dd18316100e27f4e37dbe6
Anh Thu <thuleanh543@gmail.com>
2024-04-09 00:54:31 +0700

    update right part

--- components/chats/ChatDetail.vue
+++ components/chats/ChatDetail.vue
@@ -32,7 +32,8 @@ const { data } = useAuth()
 const auth = data.value
 const messageReceived = toRef(props, 'messageReceived')
 const userRecipient = toRef(props, 'userRecipient')
-// const { lgAndUp } = useDisplay()
+
+const { lgAndUp } = useDisplay()
 //
 // const store = useChatStore()
 // onMounted(() => {
@@ -43,11 +44,11 @@ const userRecipient = toRef(props, 'userRecipient')
 //   return store.chats[store.chatContent - 1]
 // })
 //
-// const Rpart = ref(!!lgAndUp)
-//
-// function toggleRpart() {
-//   Rpart.value = !Rpart.value
-// }
+const Rpart = ref(!!lgAndUp)
+
+function toggleRpart() {
+  Rpart.value = !Rpart.value
+}
 
 const chatDetail = ref([])
 const chatContainer = ref(null)
@@ -158,129 +159,131 @@ const closeDialogForward = () => {
           </div>
         </div>
         <!---Topbar Icons-->
-        <!--        <div class="ml-auto d-flex">-->
-        <!--          <v-btn class="text-medium-emphasis" icon variant="text">-->
-        <!--            <PhoneIcon size="24" />-->
-        <!--          </v-btn>-->
-        <!--          <v-btn class="text-medium-emphasis" icon variant="text">-->
-        <!--            <VideoPlusIcon size="24" />-->
-        <!--          </v-btn>-->
-        <!--          <v-btn class="text-medium-emphasis" icon variant="text" @click="toggleRpart">-->
-        <!--            <DotsVerticalIcon size="24" />-->
-        <!--          </v-btn>-->
-        <!--        </div>-->
+        <div class="ml-auto d-flex">
+          <v-btn class="text-medium-emphasis" icon variant="text">
+            <PhoneIcon size="24" />
+          </v-btn>
+          <v-btn class="text-medium-emphasis" icon variant="text">
+            <VideoPlusIcon size="24" />
+          </v-btn>
+          <v-btn class="text-medium-emphasis" icon variant="text" @click="toggleRpart">
+            <DotsVerticalIcon size="24" />
+          </v-btn>
+        </div>
         <!---Topbar Icons-->
       </div>
       <v-divider />
       <!---Chat History-->
-      <perfect-scrollbar ref="chatContainer" class="rightpartHeight">
+      <div ref="chatContainer" class="rightpartHeight">
         <div class="d-flex">
           <div class="w-100">
-            <div v-for="chat in chatDetail" :key="chat.id" class="pa-5">
-              <div class="messages-container" @mouseenter="showMenu(chat.id)" @mouseleave="closeMenu(chat.id)">
-                <div v-if="auth?.id === chat.senderId" class="justify-end d-flex text-end mb-1">
-                  <div>
-                    <small v-if="chat.createdAt" class="text-medium-emphasis text-subtitle-2">
-                      {{
-                        formatDistanceToNowStrict(new Date(chat.timestamp), {
-                          addSuffix: false,
-                        })
-                      }}
-                      ago
-                    </small>
-                    <v-row>
-                      <div v-show="isMenuVisible(chat.id)">
-                        <v-menu v-model="myOptionsMsg[chat.id]" attach location="start">
-                          <template #activator="{ props }">
-                            <v-btn
-                              v-bind="props"
-                              class="text-medium-emphasis message-menu"
-                              icon
-                              size="42"
-                              style="margin-right: 10px"
-                              variant="text"
-                              @click="openMyOptionsMsg(chat.id)"
-                            >
-                              <DotsVerticalIcon size="24" />
-                            </v-btn>
-                          </template>
-                          <v-sheet style="text-align: left">
-                            <v-list>
-                              <v-list-item @click="copyMsg(chat.id)">Copy</v-list-item>
-                              <v-list-item @click="forwardMsg(chat)">Forward</v-list-item>
-                              <v-list-item @click="withdrawMsg(chat.id)">Withdraw</v-list-item>
-                              <v-list-item @click="deleteMsg(chat.id)">Delete</v-list-item>
-                            </v-list>
-                          </v-sheet>
-                        </v-menu>
-                      </div>
-                      <v-sheet v-if="chat.attachments == null" class="bg-grey100 rounded-md px-3 py-2 mb-1">
-                        <p class="text-body-1">{{ chat.content }}</p>
-                      </v-sheet>
-                      <v-sheet v-else class="mb-1">
-                        <img alt="pro" class="rounded-md" :src="chat.msg" width="250" />
-                      </v-sheet>
-                    </v-row>
+            <perfect-scrollbar class="rightpartHeight">
+              <div v-for="chat in chatDetail" :key="chat.id" class="pa-5">
+                <div class="messages-container" @mouseenter="showMenu(chat.id)" @mouseleave="closeMenu(chat.id)">
+                  <div v-if="auth?.id === chat.senderId" class="justify-end d-flex text-end mb-1">
+                    <div>
+                      <small v-if="chat.createdAt" class="text-medium-emphasis text-subtitle-2">
+                        {{
+                          formatDistanceToNowStrict(new Date(chat.timestamp), {
+                            addSuffix: false,
+                          })
+                        }}
+                        ago
+                      </small>
+                      <v-row>
+                        <div v-show="isMenuVisible(chat.id)">
+                          <v-menu v-model="myOptionsMsg[chat.id]" attach location="start">
+                            <template #activator="{ props }">
+                              <v-btn
+                                v-bind="props"
+                                class="text-medium-emphasis message-menu"
+                                icon
+                                size="42"
+                                style="margin-right: 10px"
+                                variant="text"
+                                @click="openMyOptionsMsg(chat.id)"
+                              >
+                                <DotsVerticalIcon size="24" />
+                              </v-btn>
+                            </template>
+                            <v-sheet style="text-align: left">
+                              <v-list>
+                                <v-list-item @click="copyMsg(chat.id)">Copy</v-list-item>
+                                <v-list-item @click="forwardMsg(chat)">Forward</v-list-item>
+                                <v-list-item @click="withdrawMsg(chat.id)">Withdraw</v-list-item>
+                                <v-list-item @click="deleteMsg(chat.id)">Delete</v-list-item>
+                              </v-list>
+                            </v-sheet>
+                          </v-menu>
+                        </div>
+                        <v-sheet v-if="chat.attachments == null" class="bg-grey100 rounded-md px-3 py-2 mb-1">
+                          <p class="text-body-1">{{ chat.content }}</p>
+                        </v-sheet>
+                        <v-sheet v-else class="mb-1">
+                          <img alt="pro" class="rounded-md" :src="chat.msg" width="250" />
+                        </v-sheet>
+                      </v-row>
+                    </div>
                   </div>
-                </div>
-                <div v-else class="d-flex align-items-start gap-3 mb-1">
-                  <!---User Avatar-->
-                  <!-- <v-avatar>
+                  <div v-else class="d-flex align-items-start gap-3 mb-1">
+                    <!---User Avatar-->
+                    <!-- <v-avatar>
                     <img alt="pro" :src="chatDetail.thumb" width="40" />
                   </v-avatar> -->
-                  <div>
-                    <small v-if="chat.createdAt" class="text-medium-emphasis text-subtitle-2">
-                      {{
-                        formatDistanceToNowStrict(new Date(chat.timestamp), {
-                          addSuffix: false,
-                        })
-                      }}
-                      ago
-                    </small>
-                    <v-row>
-                      <v-sheet v-if="chat.attachments == null" class="bg-grey100 rounded-md px-3 py-2 mb-1">
-                        <p class="text-body-1">{{ chat.content }}</p>
-                      </v-sheet>
-                      <v-sheet v-else class="mb-1">
-                        <img alt="pro" class="rounded-md" :src="chat.msg" width="250" />
-                      </v-sheet>
-                      <div v-show="isMenuVisible(chat.id)" class="message-menu">
-                        <v-menu v-model="optionsMsg[chat.id]" attach location="end">
-                          <template #activator="{ props }">
-                            <v-btn
-                              v-bind="props"
-                              class="text-medium-emphasis"
-                              icon
-                              size="42"
-                              style="margin-left: 10px"
-                              variant="text"
-                              @click="openOptionsMsg(chat.id)"
-                            >
-                              <DotsVerticalIcon size="24" />
-                            </v-btn>
-                          </template>
-                          <v-sheet>
-                            <v-list>
-                              <v-list-item @click="copyMsg(chat.id)">Copy</v-list-item>
-                              <v-list-item @click="forwardMsg(chat)">Forward</v-list-item>
-                              <v-list-item @click="deleteMsg(chat.id)">Delete</v-list-item>
-                            </v-list>
-                          </v-sheet>
-                        </v-menu>
-                      </div>
-                    </v-row>
+                    <div>
+                      <small v-if="chat.createdAt" class="text-medium-emphasis text-subtitle-2">
+                        {{
+                          formatDistanceToNowStrict(new Date(chat.timestamp), {
+                            addSuffix: false,
+                          })
+                        }}
+                        ago
+                      </small>
+                      <v-row>
+                        <v-sheet v-if="chat.attachments == null" class="bg-grey100 rounded-md px-3 py-2 mb-1">
+                          <p class="text-body-1">{{ chat.content }}</p>
+                        </v-sheet>
+                        <v-sheet v-else class="mb-1">
+                          <img alt="pro" class="rounded-md" :src="chat.msg" width="250" />
+                        </v-sheet>
+                        <div v-show="isMenuVisible(chat.id)" class="message-menu">
+                          <v-menu v-model="optionsMsg[chat.id]" attach location="end">
+                            <template #activator="{ props }">
+                              <v-btn
+                                v-bind="props"
+                                class="text-medium-emphasis"
+                                icon
+                                size="42"
+                                style="margin-left: 10px"
+                                variant="text"
+                                @click="openOptionsMsg(chat.id)"
+                              >
+                                <DotsVerticalIcon size="24" />
+                              </v-btn>
+                            </template>
+                            <v-sheet>
+                              <v-list>
+                                <v-list-item @click="copyMsg(chat.id)">Copy</v-list-item>
+                                <v-list-item @click="forwardMsg(chat)">Forward</v-list-item>
+                                <v-list-item @click="deleteMsg(chat.id)">Delete</v-list-item>
+                              </v-list>
+                            </v-sheet>
+                          </v-menu>
+                        </div>
+                      </v-row>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </perfect-scrollbar>
           </div>
-          <!--          <div v-if="Rpart" class="right-sidebar">-->
-          <!--            <v-sheet>-->
-          <!--              <chat-info :chat-detail="chatDetail" />-->
-          <!--            </v-sheet>-->
-          <!--          </div>-->
+          <div v-if="Rpart" class="right-sidebar">
+            <v-sheet>
+              <chat-info :chat-detail="chatDetail" />
+            </v-sheet>
+          </div>
         </div>
-      </perfect-scrollbar>
+      </div>
     </div>
     <v-divider />
     <!---Chat send-->
@@ -299,7 +302,7 @@ const closeDialogForward = () => {
 </template>
 <style lang="scss">
 .rightpartHeight {
-  height: 82vh;
+  height: 78vh;
 }
 .badg-dotDetail {
   left: -9px;
--- components/chats/ChatInfo.vue
+++ components/chats/ChatInfo.vue
@@ -5,27 +5,33 @@ import { useChatStore } from '@/stores/apps/chat'
 
 const props = defineProps({ chatDetail: Object })
 
-const totalAttachment = computed(() => {
-  return uniq(flatten(props.chatDetail?.chatHistory.map((item: any) => item.attachment))).length
-})
+// const totalAttachment = computed(() => {
+//   return uniq(flatten(props.chatDetail?.chatHistory.map((item: any) => item.attachment))).length
+// })
 
-const totalMedia = computed(() => {
-  return (
-    uniq(flatten(props.chatDetail?.chatHistory.map((item: any) => (item?.type === 'img' ? item.msg : null)))).length - 1
-  )
-})
+// const totalMedia = computed(() => {
+//   return (
+//     uniq(flatten(props.chatDetail?.chatHistory.map((item: any) => (item?.type === 'img' ? item.msg : null)))).length - 1
+//   )
+// })
 </script>
 <template>
   <div v-if="chatDetail" class="pa-6">
-    <h6 class="text-h6 mb-3">Media ({{ totalMedia }})</h6>
+    <h6 class="text-h6 mb-3">
+      Media
+      <!-- ({{ totalMedia }}) -->
+    </h6>
     <div v-for="chat in chatDetail.chatHistory" :key="chat.id">
       <div v-if="chat.type == 'img'">
         <img alt="img" :src="chat.msg" width="100" />
       </div>
     </div>
 
-    <h6 class="text-h6 mb-3 mt-7">Attachments ({{ totalAttachment }})</h6>
-    <v-sheet v-if="totalAttachment != 0">
+    <h6 class="text-h6 mb-3 mt-7">
+      Attachments
+      <!-- ({{ totalAttachment }}) -->
+    </h6>
+    <!-- <v-sheet v-if="totalAttachment != 0">
       <div v-for="chat in chatDetail.chatHistory" :key="chat.id">
         <div v-for="atch in chat.attachment" :key="atch.file" class="d-flex align-center mt-7">
           <v-avatar class="rounded-md bg-grey100" size="48">
@@ -37,8 +43,9 @@ const totalMedia = computed(() => {
           </div>
         </div>
       </div>
-    </v-sheet>
-    <v-sheet v-else>
+    </v-sheet> -->
+    <!-- <v-sheet v-else> -->
+    <v-sheet>
       <v-alert class="text-subtitle-1" color="error" density="compact" type="warning" variant="tonal">
         No Attachments Found!
       </v-alert>
